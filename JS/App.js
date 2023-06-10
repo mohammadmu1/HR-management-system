2d8c8d63b7e916dda7c4b427f8a9c296db7fb1be
mohammadmu1 <mbn.1991@yahoo.com>
2023-06-10 09:44:31 +0000

    hr

--- JS/App.js
+++ JS/App.js
@@ -1,8 +1,8 @@
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  let employeeArr=[] 
+    }
+
+let employeeArr=[] 
 
 class employee{
     constructor(ID , FullName , Department , Level ){
