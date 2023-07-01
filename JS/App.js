ee485b398984c9dcee7548986d3c25e8c47ff28d
mohammadmu1 <mbn.1991@yahoo.com>
2023-07-01 16:21:41 +0000

    localStorage

--- JS/App.js
+++ JS/App.js
@@ -1,57 +1,65 @@
-'use strict';
 
-let employeeArr = []; // Creating an array to store employee instances
+let emp=[];
+// localStorage.clear();
+// save objects to Locala Storage
+function saveDataToLocalaStorage(){
 
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-class Employee{
-    constructor(EmployeeID, FullName, Department, Level, ImageURL) {
-    this.EmployeeID = EmployeeID;
-    this.FullName = FullName;
-    this.Department = Department;
-    this.Level = Level;
-    this.ImageURL = ImageURL;
-    this.Salary = 0;
-    employeeArr.push(this);
-    this.netSalary = function() {
-    this.Salary = this.Salary - this.Salary * 0.075;
+  let stringData=JSON.stringify(emp);
+  localStorage.setItem('employeesData' , stringData);
+  localStorage.setItem('num' , num.toString());
   }
+
+  // retrive objects from Locala Storage
+  function getDataFromLocalaStorageToRender(){
+    let stringData=localStorage.getItem('employeesData');
+    arrObj=JSON.parse(stringData);
+    
+    if(arrObj != null){
+    for(let i=0 ; i<arrObj.length;i++){
+    //(EmployeeID, FullName, Department, Level, ImageURL,Salary)
+    new employee(arrObj[i].EmployeeID , arrObj[i].FullName , arrObj[i].Department,arrObj[i].Level,
+      arrObj[i].ImageURL,arrObj[i].Salary);
+      emp[i].setNetSalary();
+      num=Number(localStorage.getItem('num'));
+    }
+    
+  }
+    
+  }
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+class employee{
+  constructor(EmployeeID, FullName, Department, Level, ImageURL,Salary) {
+  this.EmployeeID = EmployeeID;
+  this.FullName = FullName;
+  this.Department = Department;
+  this.Level = Level;
+  this.ImageURL = ImageURL;
+  this.Salary = Salary;
+  this.netSalary = 0;
+  emp.push(this)
+}
+
+
+setNetSalary() {
+  this.netSalary=Math.round(( this.Salary - this.Salary * 0.075) * 100) / 100;
 }
 };
 
-
-Employee.prototype.randomSalary = function() {
-  if (this.Level === "Junior") {
-    this.Salary = getRandomNumber(500 , 1000);
-  }
-  if (this.Level === "Mid-Senior") {
-    this.Salary = getRandomNumber(1000,1500 );
-  }
-  if (this.Level === "Senior") {
-    this.Salary = getRandomNumber(1500 , 2000);
-  }
+//generate salary depend on experince level
+employee.prototype.randomSalary = function() {
+if (this.Level === "Junior") {
+  this.Salary = getRandomNumber(500 , 1000);
+}
+if (this.Level === "Mid-Senior") {
+  this.Salary = getRandomNumber(1000,1500 );
+}
+if (this.Level === "Senior") {
+  this.Salary = getRandomNumber(1500 , 2000);
+}
 };
-
-
-Employee.prototype.renderEmployee = function() {
-  let empData = document.getElementById("employeeData");
-  let img = document.createElement("img");
-  img.src = "assets/emp.png";
-  img.width = 85;
-  let empName = document.createElement("p");
-  empName.textContent = this.FullName;
-  let departmentLevel = document.createElement("p");
-  departmentLevel.textContent = "Department: " + this.Department + " - Level: " + this.Level;
-  let empId = document.createElement("p");
-  empId.textContent = this.EmployeeID;
-  empData.appendChild(img);
-  empData.appendChild(empName);
-  empData.appendChild(departmentLevel);
-  empData.appendChild(empId);
-};
-
-
+// ID generator
 let num = 0;
 function idGenerator(num) {
   let str = num.toString();
@@ -60,21 +68,58 @@ function idGenerator(num) {
   return id;
 }
 
-
+//chose the form to set event on it
 const myForm = document.getElementById("myForm");
-
-
-myForm.addEventListener("submit", submitHandler);
-
-function submitHandler(event) {
+//start of event 
+myForm.addEventListener("submit", (event)=> {
   event.preventDefault();
-  console.log(event);
-  let fullname = event.target.Name.value;
-  let department = event.target.Department.value;
-  let level = event.target.Level.value;
+  
+  let fullname = document.getElementById('Name').value;
+  let department =document.getElementById("Department").value;
+  let level = document.getElementById("Level").value;
   num++;
-  let registeredEmployee = new Employee(idGenerator(num), fullname, department, level);
+  let registeredEmployee = new employee(idGenerator(num), fullname, department, level);
   registeredEmployee.randomSalary();
-  registeredEmployee.netSalary();
+  registeredEmployee.setNetSalary();
+  saveDataToLocalaStorage();
   registeredEmployee.renderEmployee();
+  });
+  
+
+//render employee data that taken from user in html page 
+employee.prototype.renderEmployee=function(){
+let empData = document.getElementById("employeeData");
+let img = document.createElement("img");
+img.src = "assets/emp.png";
+img.width = 85;
+let empName = document.createElement("p");
+empName.textContent = this.FullName;
+let departmentLevel = document.createElement("p");
+departmentLevel.textContent = "Department: " + this.Department + " - Level: " + this.Level;
+let empId = document.createElement("p");
+empId.textContent = `ID : ${this.EmployeeID}`;
+let empSalary = document.createElement("p");
+empSalary.textContent = `the net salary is : ${this.netSalary}`;
+empData.appendChild(img);
+empData.appendChild(empName);
+empData.appendChild(departmentLevel);
+empData.appendChild(empId);
+empData.appendChild(empSalary);
 }
+
+function renderAll(){
+  let length=emp.length;
+  for(let i=0;i<length;i++){
+    emp[i].renderEmployee();
+  }
+}
+getDataFromLocalaStorageToRender();
+renderAll();
+let cBtn=document.getElementById('clearButton');
+
+cBtn.addEventListener("click",(event)=>{
+  localStorage.clear();
+  location.reload();
+})
+
+
